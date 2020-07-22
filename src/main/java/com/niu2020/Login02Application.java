3715b13044917a15cb3058883e41943235114455
javier mejia <proyectb2.1@gmail.com>
2020-07-21 19:28:20 -0600

    login creacion de entidades tablas de roles y usuarios coneccion a bases de datos

---
+++ src/main/java/com/niu2020/Login02Application.java
@@ -0,0 +1,13 @@
+package com.niu2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Login02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Login02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/niu2020/Login02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niu2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Login02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
